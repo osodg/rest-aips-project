bbd19f7d6964bf63f955feed8ba0a480197adc4f
Mark Fuentes <osodg@yahoo.com>
2023-11-22 11:01:42 -0500

    Fix deplyment issues

--- app.py
+++ app.py
@@ -25,7 +25,10 @@ def create_app(db_url=None):
   app.config["OPENAPI_URL_PREFIX"] = "/"
   app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
   app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-  app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+  if os.environ.get("FLASK_ENV") == "production":
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/data.db"
+  else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
   app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
   db.init_app(app)
   migrate = Migrate(app, db)
